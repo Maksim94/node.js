e3d72aa9c33248850f2980836a6fbf8b12f0a822
Maxim <maxim.kondzera@gmail.com>
2016-10-04 22:05:56 +0300

    PubSub was added

--- 3/studentsList&palette (node js)/client/libs/mediator.js
+++ 3/studentsList&palette (node js)/client/libs/mediator.js
@@ -1,12 +1,1 @@
-var mediator = {
-    events: {},
-    sub: function (eventName, fn) {
-        this.events[eventName] = this.events[eventName] || [];
-        this.events[eventName].push(fn);
-    },
-    pub: function (eventName, data) {
-        this.events[eventName].forEach(function (fn) {
-            fn(data);
-        });
-    }
-};
+var mediator = new PubSub();
